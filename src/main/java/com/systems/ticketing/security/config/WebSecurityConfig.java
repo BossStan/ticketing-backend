67bdd7efa62c75acc44e0731a0b5158c651d8f5b
Andrew Munhanga <andrewmunhanga@gmail.com>
2022-09-08 11:25:51 +0200

    feat: added OpenApi and Swagger for documentations for the APIs fix: added a generic profile which doesn't expose sensitive data

--- src/main/java/com/systems/ticketing/security/config/WebSecurityConfig.java
+++ src/main/java/com/systems/ticketing/security/config/WebSecurityConfig.java
@@ -24,7 +24,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/api/v*/registration/**","/form**","/ticket/api/v1/**")
+                    .antMatchers("/api/v*/registration/**","/form**","/ticket/api/v1/**",
+                            "/v3/**",
+                            "/swagger-ui/**"
+                            )
                     .permitAll()
                 .anyRequest()
                 .authenticated().and()
